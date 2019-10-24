49bc70507c0bdc1abe1d5ac7e62b8e2f0ede49ce
Tjebbe <tjebbemarchand@icloud.com>
2019-10-24 15:19:27 +0200

    Added Chroma package and colorHelpers file to generate different kind of colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Palette from './Pallete';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 class App extends Component {
   render() {
