635da2b5b92f012a1a17678456860edb6fec9212
Tjebbe <tjebbemarchand@icloud.com>
2019-12-22 18:20:35 +0100

    Updates

---
+++ precache-manifest.a4fa0099fc9cb220ce17d2fa313b6a84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fe2366136bbe5780b8a4d359d23573",
+    "url": "/colors-app/index.html"
+  },
+  {
+    "revision": "a8ae4ae6d85113a361a1",
+    "url": "/colors-app/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "42c438eb99141487eaad",
+    "url": "/colors-app/static/css/main.97e9f2a1.chunk.css"
+  },
+  {
+    "revision": "a8ae4ae6d85113a361a1",
+    "url": "/colors-app/static/js/2.c2314d67.chunk.js"
+  },
+  {
+    "revision": "42c438eb99141487eaad",
+    "url": "/colors-app/static/js/main.8d55c080.chunk.js"
+  },
+  {
+    "revision": "1020afd3eea0412a44b7",
+    "url": "/colors-app/static/js/runtime-main.ffe96785.js"
+  },
+  {
+    "revision": "b38b8d21b552973892f4ca20250b566f",
+    "url": "/colors-app/static/media/bg.b38b8d21.svg"
+  }
+]);
